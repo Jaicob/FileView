e0881a3f35041be5889a1693e5aba2550b3e852c
Jaicob <jaicob@icloud.com>
2014-08-28 23:47:16 -0400

    Initial Commit

---
+++ FileView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FileView
+//
+//  Created by Jaicob Stewart on 8/28/14.
+//  Copyright (c) 2014 Jaicob Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCSAppDelegate class]));
+  }
+}
---
+++ FileViewTests/FileViewTests.m
@@ -0,0 +1,34 @@
+//
+//  FileViewTests.m
+//  FileViewTests
+//
+//  Created by Jaicob Stewart on 8/28/14.
+//  Copyright (c) 2014 Jaicob Stewart. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FileViewTests : XCTestCase
+
+@end
+
+@implementation FileViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
